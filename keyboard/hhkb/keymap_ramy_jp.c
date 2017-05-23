1ae93a8c5752ce8281b38b71fe2e95debd43baf9
Ramy Sadek <ramy.sadek@jhuapl.edu>
2017-05-23 12:02:34 -0400

    WIP:\n

--- keyboard/hhkb/keymap_ramy_jp.c
+++ keyboard/hhkb/keymap_ramy_jp.c
@@ -13,7 +13,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP_JP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, \
               TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, \
               LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN3,QUOT,FN5,PENT, \
-              FN0,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,  UP,  FN0, \
+              LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,  UP,  FN0, \
               FN0, LALT,LGUI,FN2 ,FN1,    SPC,  BSPC, RCTL,RALT,RGUI, LEFT,DOWN,RGHT),
 
     /* Layer 1: HHKB mode (HHKB Fn)
